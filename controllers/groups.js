894f54fb774b2d1d2e23e82556ae652f61a24384
OnePartHarmony <bakingharmony@gmail.com>
2022-10-11 10:31:41 -0400

    added in some boilerplate views

--- controllers/groups.js
+++ controllers/groups.js
@@ -22,42 +22,75 @@ router.get("/", (req,res) => {
 
 /////////GET route to render new group form///////////
 router.get("/new", (req,res) => {
-
+    const username = req.session.username
+    const loggedIn = req.session.loggedIn
+    const userId = req.session.userId
+    const email = req.session.email
+    res.render("groups/new", {username, loggedIn, userId, email})
 })
 
 /////////POST route to create new group///////////////
 router.post("/new", (req,res) => {
-    
+    req.body.owner = req.session.userId
+    Group.create(req.body)
+        .then(group => {
+            res.redirect("/groups")
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 })
 
 /////GET route to render show group page////////////
 router.get("/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId
+    const username = req.session.username
+    const loggedIn = req.session.loggedIn
+    const userId = req.session.userId
+    const email = req.session.email
+    const currentGroup = req.session.groupId
+    Group.findById(groupId)
+        .then(group => {
+            res.render("/groups/show", {group, username, loggedIn, userId, email, currentGroup})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 })
 
 ////////POST route to work as group///////////////
 router.post("/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId    
+    Group.findById(groupId)
+        .then(group => {
+            if (group.members.includes(req.session.email)) {
+                req.session.groupId = groupId
+                res.redirect("/songs")
+            } else {
+                res.redirect(`/error?error=not%20a%20member%20of%20${group.name}`)
+            }
+        })
+        .catch(err => { res.redirect(`/error?error=${err}`)})
 })
 
 //////////GET route to render edit group form////////////
 router.get("/edit/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId
+
 })
 
 ///////PUT route to update group///////////////
 router.put("/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId
+
 })
 
 ////////GET route to render delete group page////////////
 router.get("/delete/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId
+
 })
 
 ///////////DELETE route to delete group////////////
 router.delete("/:groupId", (req,res) => {
-    
+    const groupId = req.params.groupId
+
 })
 
 
--- controllers/songs.js
+++ controllers/songs.js
@@ -14,7 +14,12 @@ router.get("/", (req,res) => {
 
 /////////GET route to render new song form///////////
 router.get("/new", (req,res) => {
-
+    const username = req.session.username
+    const loggedIn = req.session.loggedIn
+    const userId = req.session.userId
+    const email = req.session.email
+    const currentGroup = req.session.groupId
+    res.render("songs/new", {username, loggedIn, userId, email, currentGroup})
 })
 
 ////////POST route to create new song////////////
@@ -24,26 +29,31 @@ router.post("/new", (req,res) => {
 
 ////////GET route to show song///////////
 router.get("/:songId", (req,res) => {
+    const songId = req.params.songId
 
 })
 
 //////////GET route to render edit song form////////////
 router.get("/edit/:songId", (req,res) => {
+    const songId = req.params.songId
 
 })
 
 ///////PUT route to update song///////////////
 router.put("/:songId", (req,res) => {
+    const songId = req.params.songId
 
 })
 
 ////////GET route to render delete song page////////////
 router.get("/delete/:songId", (req,res) => {
+    const songId = req.params.songId
 
 })
 
 ///////////DELETE route to delete song////////////
 router.delete("/delete/:songId", (req,res) => {
+    const songId = req.params.songId
 
 })
 
